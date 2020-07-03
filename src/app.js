1a9057b8624dd5d0f074bd175dad499bc3b6421f
Victor Rocha <vrsarmento@gmail.com>
2020-07-03 20:46:12 -0300

    Counter initial implementation

--- src/app.js
+++ src/app.js
@@ -1,31 +1,10 @@
 'use strict'
 
-import React, { Component } from 'react'
-//import Example from 'components/example'
-import './css/style.css'
+import React from 'react'
+import CounterContainer from 'components/counter'
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      title: 'My React App',
-      Component: 'span'
-    }
-  }
-
-  async componentDidMount () {
-    // Dynamic import example - Another way to code splitting 
-    const title = await import('components/example')
-    this.setState({
-      Component: title.default
-    })
-  }
-
-  render () {
-    return (
-      <this.state.Component>{this.state.title}</this.state.Component>
-    )
-  }
-}
+const App = ({ store }) => (
+  <CounterContainer store={store} />
+)
 
 export default App
---
+++ src/components/counter/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+import Counter from './counter'
+
+class CounterContainer extends PureComponent {
+  constructor () {
+    super()
+
+    this.increment = () => this.props.store.dispatch({ type: 'INCREMENT' })
+
+    this.decrement = () => this.props.store.dispatch({ type: 'DECREMENT' })
+  }
+
+  componentDidMount () {
+    this.unsubscribe = this.props.store.subscribe(() => this.forceUpdate())
+  }
+
+  componentWillUnmount () {
+    this.unsubscribe()
+  }
+
+  render () {
+    return (
+      <Counter counter={this.props.store.getState()} decrement={this.decrement} increment={this.increment} />
+    )
+  }
+}
+
+export default CounterContainer
