557edfe22af82ff7634492b1531ef4411e7cff72
Victor Rocha <vrsarmento@gmail.com>
2020-07-15 18:13:59 -0300

    Reducer Todos test

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 'use strict'
 
-import React  from 'react'
+import React from 'react'
 import './css/style.css'
 
 const App = () => (
   <div>
-    <input type="text" />
+    <input type='text' />
+
     <ul>
-      <li>Item 1</li>
+      <li className='completed'>Item 1</li>
       <li>Item 2</li>
       <li>Item 3</li>
       <li>Item 4</li>
       <li>Item 5</li>
     </ul>
+
+    <div>
+      <h3>Exibir</h3>
+      <a href=''>Todos</a> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
+    </div>
   </div>
 )
 
