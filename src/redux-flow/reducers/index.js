1905c496a8911653988a608471489bd179e68efb
Victor Rocha <vrsarmento@gmail.com>
2020-07-22 21:40:02 -0300

    Actions names padronization

--- src/redux-flow/reducers/index.js
+++ src/redux-flow/reducers/index.js
@@ -1,17 +1,18 @@
 'use strict'
 
-// import { combineReducers } from 'redux'
+import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibility-filter'
 
-const combineReducers = (reducers) => (state = {}, action) => {
+// Manual implementation to understand how combineReducers works
+/* const combineReducers = (reducers) => (state = {}, action) => {
   return Object.keys(reducers).reduce((nextState, key) => {
     return {
       ...nextState,
       [key]: reducers[key](state[key], action)
     }
   }, {})
-}
+} */
 
 export default combineReducers({
   todos,
