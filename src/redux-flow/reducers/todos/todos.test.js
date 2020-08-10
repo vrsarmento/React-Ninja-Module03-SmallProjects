dda0f2f108bb92d290ef4d0cf4b90f7dbc698432
Victor Rocha <vrsarmento@gmail.com>
2020-08-10 19:46:19 -0300

    createReducer tests || Excluding tests that belong to createReducer initially included in reducers

--- src/redux-flow/reducers/todos/todos.test.js
+++ src/redux-flow/reducers/todos/todos.test.js
@@ -2,12 +2,8 @@
 
 import { expect } from 'chai'
 import deepFreeze from 'deep-freeze'
-import todos, { initialState } from './index'
+import todos from './index'
 import { ADD_TODO, TOGGLE_TODO } from './actions'
-
-it('should todos be a function', () => {
-  expect(todos).to.be.a('function')
-})
 
 it('should add a todo item', () => {
   const before = deepFreeze([])
@@ -58,27 +54,5 @@ it('should toggle second todo', () => {
     { id: 0, text: 'Test', completed: false },
     { id: 1, text: 'Test 1', completed: true },
   ]
-  expect(todos(before, action)).to.be.deep.equal(after)
-})
-
-it('should return the latest state when action is unknown', () => {
-  const before = deepFreeze([
-    { id: 0, text: 'Test', completed: false },
-    { id: 1, text: 'Test 1', completed: false },
-  ])
-  const action = deepFreeze({
-    type: 'UNKNOWN'
-  })
-  const after = [
-    { id: 0, text: 'Test', completed: false },
-    { id: 1, text: 'Test 1', completed: false },
-  ]
-  expect(todos(before, action)).to.be.deep.equal(after)
-})
-
-it('should return initialState when before is undefined', () => {
-  const before = undefined
-  const action = deepFreeze({})
-  const after = initialState
   expect(todos(before, action)).to.be.deep.equal(after)
 })
--- src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
+++ src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
@@ -1,13 +1,9 @@
 'use strict'
 
-import visibilityFilter, { initialState } from './index'
+import visibilityFilter from './index'
 import * as actions from './actions'
 import { expect } from 'chai'
 import deepFreeze from 'deep-freeze'
-
-it('visibilityFilter should be a function', () => {
-  expect(visibilityFilter).to.be.a('function')
-})
 
 it('should show all todos', () => {
   const before = actions.SHOW_COMPLETED
@@ -36,22 +32,5 @@ it('should show active todos', () => {
     payload: { filter: actions.SHOW_ACTIVE }
   })
   const after = actions.SHOW_ACTIVE
-  expect(visibilityFilter(before, action)).to.be.equal(after)
-})
-
-it('should return latest state when action is unknown', () => {
-  const before = actions.SHOW_ALL
-  const action = deepFreeze({
-    type: 'UNKNOWN',
-    payload: { filter: actions.SHOW_COMPLETED }
-  })
-  const after = actions.SHOW_ALL
-  expect(visibilityFilter(before, action)).to.be.equal(after)
-})
-
-it('should return initialState when latest state is undefined', () => {
-  const before = undefined
-  const action = deepFreeze({})
-  const after = initialState
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
