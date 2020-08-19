fbed3623d7cf6e7cb4401a2a36e61d5f11a20785
Victor Rocha <vrsarmento@gmail.com>
2020-08-18 21:38:28 -0300

    Async actions in Redux || SearchCep adjustments

--- src/components/search-cep/index.js
+++ src/components/search-cep/index.js
@@ -1,44 +1,26 @@
 'use strict'
 
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
-import ajax from '@fdaciuk/ajax'
 import SearchCep from './search-cep'
-import { updateAddress } from 'reducers/address/action-creators'
+import { fetchAddress } from 'reducers/address/action-creators'
 
-class SearchCepContainer extends PureComponent {
-  state = {
-    isFetching: false
-  }
-
-  handleSubmit = async (e) => {
-    e.preventDefault()
-    this.setState({ isFetching: true })
-    const response = await ajax().get('https://ws.apicep.com/cep.json', {
-      code: e.target.cep.value
-    })
-    this.setState({ isFetching: false })
-    this.props.updateAddress(response)
-  }
-
-  render () {
-    return (
-      <SearchCep
-        {...this.state}
-        address={this.props.address}
-        handleSubmit={this.handleSubmit}
-      />
-    )
-  }
-}
+const SearchCepContainer = ({ address, handleSubmit }) => (
+  <SearchCep
+    address={address}
+    handleSubmit={handleSubmit}
+  />
+)
 
 const mapStateToProps = (state) => ({
   address: state.address
 })
 
-// Caso o nome da propriedade seja o mesmo da função e os parâmetros dessa função forem os mesmos enviados para o actionCreator, o mapDispatchToProps pode ser feito seguindo este padrão (um objeto, ao invés de uma função que retorna um objeto)
-const mapDispatchToProps = {
-  updateAddress
-}
+const mapDispatchToProps = (dispatch) => ({
+  handleSubmit: (e) => {
+    e.preventDefault()
+    dispatch(fetchAddress(e.target.cep.value))
+  },
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchCepContainer)
--- src/redux-flow/reducers/address/action-creators.js
+++ src/redux-flow/reducers/address/action-creators.js
@@ -1,6 +1,14 @@
 'use strict'
 
+import ajax from '@fdaciuk/ajax'
 import { UPDATE_ADDRESS } from './actions'
+
+export const fetchAddress = (cep) => async (dispatch, getState) => {
+  const response = await ajax().get('https://ws.apicep.com/cep.json', {
+    code: cep
+  })
+  dispatch(updateAddress(response))
+}
 
 export const updateAddress = (data) => ({
   type: UPDATE_ADDRESS,
